d71c9c1aed0c001a75621df27186598fb581fbae
dutra1243 <facundodutraextra@gmail.com>
2024-11-27 04:11:49 -0300

    profile styles somewhat working, pretty ugly still

--- mobile/components/ProfileComponents/UserInfo.tsx
+++ mobile/components/ProfileComponents/UserInfo.tsx
@@ -7,6 +7,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useEffect, useState } from 'react'
 import { logoutThunk } from '@/store/authSlice'
 import { router } from 'expo-router'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 
 const UserInfo = (props: { posts: PostDTO[], user: UserDto }) => {
@@ -32,19 +33,22 @@ const UserInfo = (props: { posts: PostDTO[], user: UserDto }) => {
 
 
     return (
-        <View>
-            {props.user.profilePicture !== "" ? <Image style={styles.image} source={{ uri: props.user.profilePicture }} ></Image>
-                : <Image style={styles.image} source={{ uri: genericProfilePicture }} ></Image>}
-            <View>
-                <Text>{props.user.username}</Text>
-                {user && props.user.email === user.email ? 
-                <View>
-                    <Button title="Edit profile" ></Button>
-                    <Button title="Logout" onPress={handleLogout}></Button>
+        <View style={{}} >
+            <View style={{flexDirection: "row", gap: 50}} >
+
+                {props.user.profilePicture !== "" ? <Image style={styles.image} source={{ uri: props.user.profilePicture }} ></Image>
+                    : <Image style={styles.image} source={{ uri: genericProfilePicture }} ></Image>}
+                <View style={{flexDirection: "row", gap: 25}}>
+                    <Text>{props.user.username}</Text>
+                    {user && props.user.email === user.email ? 
+                    <View style={{flexDirection: "row", gap: 20}} >
+                        <MaterialCommunityIcons size={40} name="account-edit" ></MaterialCommunityIcons>
+                        <MaterialCommunityIcons size={40} name="logout" onPress={handleLogout}></MaterialCommunityIcons>
+                    </View>
+                    : <MaterialCommunityIcons size={40} name="account-plus" ></MaterialCommunityIcons>}
                 </View>
-                : <Button title="Add friend" ></Button>}
             </View>
-            <View>
+            <View style={{flexDirection: "row", gap: 10}} >
                 <Text>{props.posts.length} posts</Text>
                 <Text>{props.user.friends.length} friends</Text>
             </View>
@@ -55,9 +59,69 @@ const UserInfo = (props: { posts: PostDTO[], user: UserDto }) => {
 export default UserInfo
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 10,
+    },
+    profileInfo: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+    },
     image: {
         width: 80,
         height: 80,
-        borderRadius: 50
-    }
-})
+        borderRadius: 40,
+    },
+    userInfo: {
+        marginLeft: 10,
+    },
+    username: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    stats: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        paddingVertical: 10,
+    },
+    stat: {
+        alignItems: 'center',
+    },
+    statNumber: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    statLabel: {
+        fontSize: 14,
+        color: '#888',
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        paddingVertical: 10,
+    },
+    button: {
+        flex: 1,
+        marginHorizontal: 5,
+    },
+    posts: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    post: {
+        width: '33%',
+        aspectRatio: 1,
+        padding: 1,
+    },
+    postImage: {
+        width: '100%',
+        height: '100%',
+    },
+});
--- mobile/components/ProfileComponents/UserPictures.tsx
+++ mobile/components/ProfileComponents/UserPictures.tsx
@@ -8,8 +8,8 @@ const UserPictures = ({posts} : { posts : PostDTO[]}) => {
     console.log("POSTSSSSSSSSSSSSSSSSSS", posts)
 
     return (
-        <View >
-            <FlatList data={posts} renderItem={({ item }) => <Image style={styles.image} source={{ uri: item.imageUrl }} ></Image>} ></FlatList>
+        <View style={{maxHeight : 600, flexDirection: "column", flexWrap: "wrap"}} >
+            <FlatList numColumns={3} data={posts} renderItem={({ item }) => <Image style={styles.image} source={{ uri: item.imageUrl }} ></Image>} ></FlatList>
         </View>
     )
 }
@@ -20,5 +20,6 @@ const styles = StyleSheet.create({
     image: {
         width: 100,
         height: 100,
+        margin: 10
     }
 })
