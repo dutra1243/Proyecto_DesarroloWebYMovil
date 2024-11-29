98acb9161713e9c585da8b7a90de3788644d5bd7
dutra1243 <facundodutraextra@gmail.com>
2024-11-29 14:57:22 -0300

    minor fix in login button

--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -6,6 +6,8 @@ import CircularProgress from '@mui/material/CircularProgress';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import {useNavigate} from "react-router-dom";
+import { Button } from '@mui/material';
+import './style.css'
 
 
 export const Login = () => {
@@ -52,12 +54,12 @@ export const Login = () => {
                     onChange={(e) => setLoginRequest({...loginRequest, password: e.target.value})}
                     error={error}
                 />
-                <button onClick={handleLogin}
+                <Button onClick={handleLogin}
                         disabled={isLoading}
                         className="form-button"
                 >
                     {isLoading ? <CircularProgress size={15}/> : 'Login'}
-                </button>
+                </Button>
                 <div style={{minHeight: '5em', minWidth: '20em', transition: 'height 0.3s ease'}}>
                     {error && <Alert severity="error">{error.message}</Alert>}
                 </div>
